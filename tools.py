f56e36f3e5de2d4007bcbf83d453a8439543304a
VladimirSinitsin <vladimir.sinitsin.ya@yandex.ru>
2021-01-21 18:50:31 +0300

    Рефакторинг

--- tools.py
+++ tools.py
@@ -3,12 +3,27 @@ import cv2
 import datetime
 import pytz
 from pyzbar import pyzbar
+from typing import List
+from aiogram.dispatcher.handler import CancelHandler
+from aiogram.dispatcher.middlewares import BaseMiddleware
 
 import db.dbms as db
 from nalog_python import NalogRuPython
 
 
 TIME_ZONE = 'Europe/Moscow'
+
+
+class AccessMiddleware(BaseMiddleware):
+    def __init__(self, users_id: List):
+        self.users_id = users_id
+        super().__init__()
+
+    async def on_process_message(self, message, _):
+        if int(message.from_user.id) not in self.users_id:
+            await message.answer("Отказано в доступе!\nДля уточнения обратись к разработчику @VSinitsin")
+            raise CancelHandler()
+
 
 
 def get_now_formatted() -> str:
